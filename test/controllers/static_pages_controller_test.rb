2ebb6d15387a33e09b8b473a56a7772b4d3a35ab
h.morooka <h.morooka@wecnrt.co.jp>
2016-09-10 12:40:19 +0000

    5chapter start

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "getリクエスト送ったら何らかのhtmlが返ってくるか？というテスト" do
+    get :home
+    assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+end
